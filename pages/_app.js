c883ded1970cf10d490b746287c5f80126596b1a
Abhishek Kumar <guptaabhishek2719@gmail.com>
2022-02-24 10:55:15 +0530

    opensea-blockchain-clone

---
+++ pages/_app.js
@@ -0,0 +1,25 @@
+import '../styles/globals.css'
+import { ThirdwebWeb3Provider } from '@3rdweb/hooks'
+
+/**
+ * The chain ID $ represents the Rinkeby network
+ * The injected connector is a web3 conection method used by Metamask
+ */
+
+const supportedChainIds = [4]
+const connectors = {
+  injected: {},
+}
+ 
+function MyApp({ Component, pageProps }) {
+  return (
+    <ThirdwebWeb3Provider
+        supportedChainIds={supportedChainIds}
+        connectors={connectors}
+    >
+      <Component {...pageProps} />
+    </ThirdwebWeb3Provider>
+  )
+}
+
+export default MyApp
